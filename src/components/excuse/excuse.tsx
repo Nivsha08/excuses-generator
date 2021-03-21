d215c0286aff3dc481935f9fa520aacfa476a49e
Niv Shani <nivshani08@gmail.com>
2021-03-21 20:39:53 +0200

    basic setup

---
+++ src/components/excuse/excuse.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./excuse.module.scss";
+
+export const Excuse = () => {
+  return <div className={styles.excuse}>Excuse!</div>;
+};
---
+++ src/components/excuses-generator/excuses-generator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./excuses-generator.module.scss";
+
+import { Excuse } from "../excuse/excuse";
+
+export const ExcusesGenerator = () => {
+  return (
+    <div>
+      hey app!
+      <Excuse />
+    </div>
+  );
+};
